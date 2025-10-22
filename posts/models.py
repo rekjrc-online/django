11e45fe609d5c0fc290722347ba3df53f2b3143a
Jason Miller <jamiller@intricity.com>
2025-10-22 15:32:58 -0400

    Update

--- posts/models.py
+++ posts/models.py
@@ -16,10 +16,8 @@ class Post(BaseModel):
         related_name='posts',
         db_index=True )
     content = models.TextField(max_length=200)
-    # Optional media attachments
     image = models.ImageField(upload_to='post_images/', blank=True, null=True)
     video_url = models.URLField(blank=True, null=True)
-    # Engagement metrics
     likes_count = models.PositiveIntegerField(default=0)
     comments_count = models.PositiveIntegerField(default=0)
     reposts_count = models.PositiveIntegerField(default=0)
