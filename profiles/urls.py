a497116604b720fb5519abd63c912ab229334ce0
Jason Miller <jamiller@intricity.com>
2025-11-03 00:54:08 -0500

    Update

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,6 +5,7 @@ app_name = 'profiles'
 
 urlpatterns = [
     path('', views.ProfilesListView.as_view(), name='profiles-list'),
+    path('build/', views.ProfileBuildView.as_view(), name='build-profile'),
     path('<int:profile_id>/', views.ProfileDetailView.as_view(), name='detail-profile'),
     path('<int:profile_id>/update/', views.ProfileUpdateView.as_view(), name='update-profile'),
     path('<int:profile_id>/delete/', views.ProfileDeleteView.as_view(), name='delete-profile'),
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import redirect
-from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from .models import Profile
 from .forms import ProfileCreateForm, ProfileEditForm
 
@@ -11,6 +13,17 @@ class ProfilesListView(LoginRequiredMixin, ListView):
     login_url = '/humans/login/'
     def get_queryset(self):
         return Profile.objects.filter(human=self.request.user).order_by('profiletype', 'displayname')
+
+class ProfileBuildView(CreateView):
+    model = Profile
+    form_class = ProfileCreateForm
+    template_name = 'profiles/profile_create.html'
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, "Your profile was created successfully!")
+        return super().form_valid(form)
+    def get_success_url(self):
+        return reverse_lazy('profiles:profile_detail', kwargs={'pk': self.object.pk})
 
 class ProfileDetailView(DetailView):
     model = Profile
