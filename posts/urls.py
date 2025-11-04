e61db991d3f3c9c61b5a4d4c328e9e69976bdd54
Jason Miller <jamiller@intricity.com>
2025-11-04 14:28:07 -0500

    Update

--- posts/urls.py
+++ posts/urls.py
@@ -1,13 +1,14 @@
+from django.views.generic import TemplateView
 from django.urls import path
 from . import views
 
 app_name = 'posts'
 
 urlpatterns = [
-    # Example placeholder view
 	path('create/', views.PostCreateView.as_view(), name='post-create'),
     path('<int:post_id>/', views.PostDetail.as_view(), name='post_detail'),
     path('<int:post_id>/reply/', views.PostReplyView.as_view(), name='post_reply'),
     path('<int:post_id>/like-ajax/', views.toggle_like_ajax, name='post_like_ajax'),
     path('<int:post_id>/replies/ajax/', views.PostRepliesAjax.as_view(), name='PostRepliesAjax'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
 ]
