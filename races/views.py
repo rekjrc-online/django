0c4c8fc35950157982dee6c63da5e22ebb6e6bd6
Jason Miller <jamiller@intricity.com>
2025-11-03 19:01:15 -0500

    Update

--- races/views.py
+++ races/views.py
@@ -1,4 +1,4 @@
-from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
