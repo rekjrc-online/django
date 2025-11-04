fecac891990c100168119104423034c71318f674
Jason Miller <jamiller@intricity.com>
2025-11-04 15:12:43 -0500

    Update

--- humans/forms.py
+++ humans/forms.py
@@ -1,3 +1,5 @@
+#from captcha.fields import ReCaptchaField
+#from captcha.widgets import ReCaptchaV2Checkbox
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Human
@@ -6,7 +8,6 @@ class HumanRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(required=False)
     last_name = forms.CharField(required=False)
-
     class Meta:
         model = Human
         fields = ['username', 'first_name', 'last_name', 'email', 'phone_number', 'password1', 'password2']
