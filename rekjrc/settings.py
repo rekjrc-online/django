060d6aea7bcfaa895f69d414520c538cb43ae541
Jason Miller <jamiller@intricity.com>
2025-11-04 20:56:53 -0500

    Update

--- rekjrc/settings.py
+++ rekjrc/settings.py
@@ -15,7 +15,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY
 # ------------------------------
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('DEBUG')
+DEBUG = False
 ALLOWED_HOSTS = ['localhost','rekjrc.com','www.rekjrc.com','10.1.1.63']
 
 # ------------------------------
--- rekjrc/urls.py
+++ rekjrc/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import handler404
 from django.conf.urls.static import static
+from django.shortcuts import render
 from posts import views as post_views
+
+def custom_404(request, exception):
+    return render(request, "404.html", status=404)
+
+handler404 = custom_404
 
 urlpatterns = [
     path('', post_views.HomepageView.as_view(), name='homepage'),
